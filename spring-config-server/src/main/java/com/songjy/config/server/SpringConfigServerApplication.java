4e0540efee7511b93d5b3dc2884b456f48a3cd26
songjy <songjy@ipharmacare.net>
2020-09-27 21:56:18 +0800

    spring config server

---
+++ spring-config-server/src/main/java/com/songjy/config/server/SpringConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.songjy.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author songjy
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class SpringConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigServerApplication.class, args);
+    }
+
+}
